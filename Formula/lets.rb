71c811d6481235df0a85082854198ab98b07be62
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-03 14:22:09 +0300

    Brew formula update for lets version v0.0.33

--- Formula/lets.rb
+++ Formula/lets.rb
@@ -5,21 +5,26 @@
 class Lets < Formula
   desc "CLI task runner for productive developers - a better alternative to make"
   homepage "https://lets-cli.org/"
-  version "0.0.32"
+  version "0.0.33"
   license "MIT"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/lets-cli/lets/releases/download/v0.0.32/lets_Darwin_x86_64.tar.gz"
-    sha256 "a413e4d8e45f6405600a54c6ed87bb7b8a556a0be12fafffedfb4599314dfc63"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Darwin_x86_64.tar.gz"
+      sha256 "1f47f565b8cb590e1b16bc70aec2bb37d8892a79697c891a90584ed83ce23acf"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Darwin_arm64.tar.gz"
+      sha256 "bbac6de83bd1c2bb6dd2fd219fceac558bd9a542e1fc3a48922e843be5a8bce8"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/lets-cli/lets/releases/download/v0.0.32/lets_Darwin_arm64.tar.gz"
-    sha256 "a2676590a0128f4f19fed357615f368cb98375315c488d0b891ccca940f738fa"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/lets-cli/lets/releases/download/v0.0.32/lets_Linux_x86_64.tar.gz"
-    sha256 "f838e6c9e3f38fed57a6cc70816e89c006ba97703ccff83b43dbeb0874d616a8"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Linux_x86_64.tar.gz"
+      sha256 "19694f806e33676f0d8f1c252147fcfd1aa1c70bc71e1ad1f8e74672be5a5511"
+    end
   end
 
   def install
