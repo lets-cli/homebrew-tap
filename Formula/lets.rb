cbab843afa802f4c44a3dcfce092228be6e18e81
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-10 12:28:29 +0200

    Brew formula update for lets version v0.0.40

--- Formula/lets.rb
+++ Formula/lets.rb
@@ -5,29 +5,36 @@
 class Lets < Formula
   desc "CLI task runner for productive developers - a better alternative to make"
   homepage "https://lets-cli.org/"
-  version "0.0.33"
+  version "0.0.40"
   license "MIT"
-  bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Darwin_x86_64.tar.gz"
-      sha256 "1f47f565b8cb590e1b16bc70aec2bb37d8892a79697c891a90584ed83ce23acf"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Darwin_arm64.tar.gz"
-      sha256 "bbac6de83bd1c2bb6dd2fd219fceac558bd9a542e1fc3a48922e843be5a8bce8"
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.40/lets_Darwin_arm64.tar.gz"
+      sha256 "97c5b7f1856d7ac38b02c5db5c41e151577839c74365e13e9862ccf598b1431f"
+
+      def install
+        bin.install "lets"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.40/lets_Darwin_x86_64.tar.gz"
+      sha256 "21e75cbf512d584bb6fb80d22b827ad406bf74da20541993d45320f426ad91ac"
+
+      def install
+        bin.install "lets"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/lets-cli/lets/releases/download/v0.0.33/lets_Linux_x86_64.tar.gz"
-      sha256 "19694f806e33676f0d8f1c252147fcfd1aa1c70bc71e1ad1f8e74672be5a5511"
-    end
-  end
+      url "https://github.com/lets-cli/lets/releases/download/v0.0.40/lets_Linux_x86_64.tar.gz"
+      sha256 "93cd9b5e52ad76832e33d4250374e39b337170f0e8e1f77ad7a85ea10b49cffd"
 
-  def install
-    bin.install "lets"
+      def install
+        bin.install "lets"
+      end
+    end
   end
 end
